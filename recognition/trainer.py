8ab9159e1cdb240e16638cc9638c07995a572cd9
Obada <obada.jabassini@gmail.com>
2017-11-19 01:19:14 +0200

    Refactor Training

--- recognition/trainer.py
+++ recognition/trainer.py
@@ -1,34 +1,50 @@
+import shutil
+import torch
 import torch.nn as nn
 from torch.autograd import Variable
 from torch.optim import Adam
 from torch.utils.data import DataLoader
-
 from recognition.face_recognition_dataset import FaceRecognitionDataset
 from recognition.net import Net
 
-faces_image_path = "../data/lfw/"
+
+def save_checkpoint(state, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+
+
+faces_image_path = "../data/lfw2/"
 dataset = FaceRecognitionDataset(faces_image_path)
-dataloader = DataLoader(dataset, batch_size=64, shuffle=True, num_workers=8)
-net = Net(len(dataset.names))
+# dataset = FaceRecognitionDataset()
+dataloader = DataLoader(dataset, batch_size=128, shuffle=True, num_workers=8)
+l = len(dataset.names)
+net = Net(l)
 net = nn.DataParallel(net).cuda()
-criterion = nn.CrossEntropyLoss()
-optimizer = Adam(net.parameters(), lr=0.001)
-epochs = 1000
-running_loss = 0
+criterion = nn.CrossEntropyLoss().cuda()
+optimizer = Adam(filter(lambda x: x.requires_grad, net.parameters()), lr=0.001)
+epochs = 100
+print("Begin Training")
+batch_loss, total_loss = 0, 0
+batches = 0
 for epoch in range(epochs):
-    for i, data in enumerate(dataloader, 0):
-        inputs, labels = data
-        inputs, labels = Variable(inputs).cuda(), Variable(labels).cuda()
+    for batch, (inputs, labels) in enumerate(dataloader, 0):
+        inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
         optimizer.zero_grad()
         outputs = net(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
-        running_loss += loss.data[0]
-        if i % 2000 == 1999:  # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
-        print("batch finished")
-    print("epoch finished")
-
+        batch_loss += loss.data[0]
+        batches += 1
+        if batch != 0 and batch % 30 == 0:
+            print("Epoch = {}, Batch = {}, Error = {}".format(epoch, batch, batch_loss))
+            total_loss += batch_loss
+            batch_loss = 0
+    save_checkpoint({
+        'epoch': epoch + 1,
+        'state_dict': net.state_dict(),
+        'optimizer': optimizer.state_dict(),
+        "num_classes": l
+    })
+    total_loss = total_loss / batches
+    print("epoch finished with loss = {}".format(total_loss))
+    total_loss, batches = 0, 0
