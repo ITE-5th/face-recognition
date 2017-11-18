1669b95937fd071941d51eed93bb906241ddd2b7
Obada <obada.jabassini@gmail.com>
2017-11-19 01:18:43 +0200

    Put the pretrained network in the class

--- recognition/net.py
+++ recognition/net.py
@@ -3,15 +3,16 @@ import torch.nn.functional as F
 
 from recognition.inceptionresnetv2 import inceptionresnetv2
 
-path_of_pretrained_model = "../data/inceptionresnetv2.pth"
-extractor = inceptionresnetv2(path_of_pretrained_model).cuda()
-for param in extractor.parameters():
-    param.requires_grad = False
+
 
 
 class Net(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
+        path_of_pretrained_model = "../data/inceptionresnetv2.pth"
+        self.extractor = inceptionresnetv2(path_of_pretrained_model)
+        for param in self.extractor.parameters():
+            param.requires_grad = False
         self.num_classes = num_classes
         self.linear1 = nn.Linear(1000, 100)
         self.dropout = nn.Dropout()
@@ -19,10 +20,9 @@ class Net(nn.Module):
         self.linear3 = nn.Linear(50, num_classes)
 
     def forward(self, x):
-        x = extractor.forward(x)
+        x = self.extractor(x)
         x = F.relu(self.linear1(x))
         x = self.dropout(x)
         x = F.relu(self.linear2(x))
-        # x = F.softmax(self.linear3(x))
         x = self.linear3(x)
         return x
