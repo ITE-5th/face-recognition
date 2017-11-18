2b706f0ffe2066faf0846d9f23175f1e7fb46896
Obada <obada.jabassini@gmail.com>
2017-11-19 01:18:21 +0200

    Refactor reading images

--- recognition/face_recognition_dataset.py
+++ recognition/face_recognition_dataset.py
@@ -1,45 +1,60 @@
-import os
-
-import cv2
-import dlib
-import numpy as np
-import openface
-import torch
-import torchvision.models as models
-from torch.utils.data.dataset import Dataset
 import glob
+import os
+from multiprocessing import Pool, cpu_count
+import cv2
+import numpy as np
+import torch
+from torch.utils.data.dataset import Dataset
+
+from preprocessing.preprocessor import Preprocessor
 
 
 class FaceRecognitionDataset(Dataset):
-    path_to_pretrained_model = "../data/shape_predictor_68_face_landmarks.dat"
-    path_to_cnn_model = "../data/mmod_human_face_detector.dat"
-    detector = dlib.cnn_face_detection_model_v1(path_to_cnn_model)
-    predictor = dlib.shape_predictor(path_to_pretrained_model)
-    aligner = openface.AlignDlib(path_to_pretrained_model)
-
-    def __init__(self, root_path: str):
+    def __init__(self, root_path: str, preprocessor: Preprocessor = None):
+        self.preprocessor = preprocessor
         self.names = [path for path in os.listdir(root_path)]
         faces_path = glob.glob(root_path + "/**/*.jpg")
-        self.faces = [(self.names.index(self.extract_name(face_path)), self.prepare_image(face_path)) for face_path in
-                      faces_path]
-        print(self.faces)
+        with Pool(cpu_count()) as p:
+            self.faces = p.map(self.extract_item, faces_path)
+            p.close()
+            p.join()
+
+    # def __init__(self):
+    #     self.preprocessor = None
+    #     self.names = ["Aaron_Eckhart", "Aaron_Guiel", "Abdullah", "Abudl_Rahman"]
+    #     faces_path = glob.glob("../data/lfw2/**/*.jpg")
+    #     faces_path = [path for path in faces_path if self.extract_name(path) in self.names]
+    #     # self.faces = [self.extract_item(item) for item in faces_path]
+    #     with Pool(cpu_count()) as p:
+    #         self.faces = p.map(self.extract_item, faces_path)
+    #         p.close()
+    #         p.join()
 
     def __len__(self):
         return len(self.faces)
 
     def __getitem__(self, index):
-        face = self.faces[index]
-        return face[1], face[0]
+        return self.faces[index]
+
+    def name_from_index(self, index: int):
+        return self.names[index]
+
+    def extract_item(self, item):
+        name = self.extract_name(item)
+        ind = self.names.index(name)
+        image = self.prepare_image(item)
+        tu = image.float(), ind
+        return tu
 
     def extract_name(self, path: str):
         return path[path.rfind("/") + 1:path.rfind("_")]
 
     def prepare_image(self, image_path: str):
         image = cv2.imread(image_path)
-        rect = FaceRecognitionDataset.detector(image, 1)[0]
-        # aligned = FaceRecognitionDataset.aligner.align(299, image, rect,
-        #                                                landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
-        # aligned = np.swapaxes(aligned, 0, 2)
-        # aligned = np.swapaxes(aligned, 1, 2)
-        # return torch.from_numpy(aligned)
-        return []
+        if self.preprocessor is None:
+            image = np.swapaxes(image, 0, 2)
+            image = np.swapaxes(image, 1, 2)
+            res = torch.from_numpy(image)
+            return res
+        aligned = self.preprocessor.preprocess(image)
+        return torch.from_numpy(aligned)
